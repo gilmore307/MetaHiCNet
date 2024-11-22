5995f5cfb123e77de9b1d309467bbbfbfebf8012
gilmore307 <sunchentong.99@qq.com>
2024-11-22 23:07:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -330,4 +330,4 @@ register_normalization_callbacks(app)
 register_visualization_callbacks(app)
 
 if __name__ == "__main__":
-    app.run_server(debug=True, host="0.0.0.0", port=8050)
+    app.run_server(debug=False, host="0.0.0.0", port=8050)
