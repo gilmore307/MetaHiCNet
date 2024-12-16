c31df7fe32cb28d1419d1afdde9d8cc3f3f5e8cd
gilmore307 <sunchentong.99@qq.com>
2024-12-16 18:00:11 +0800

    ssl

---
+++ change.py
@@ -0,0 +1,45 @@
+title 减小
+
+图用raw matrix
+
+y axix title:  row contact
+
+table normalized contacts...
+
+normalized /
+
+
+
+2. Contig coverage update according to the file
+
+3. Switch section 2 and 3
+
+4. Remove the title of raw contacts
+
+5. Raw contacts: y axis title Raw Hi-C contacts
+
+6 Table: Normalized Hi-C Contacts
+
+7. Normalized contacts divided by the non zero minimal value
+
+8. Hi-C contacts between MAG_xxx and other nodes in the network
+
+9. Hi-C contacts between f_Rhizobiaceae_P and other nodes in the network
+
+10. Delete Hi-C contacts with family in the network
+
+11. email:... if ...
+
+12. home page optimization 
+
+13. All resources and tools on this website are freely accessible to the public. For questions, please contact us at yuxuan.du@utsa.edu.
+
+14. Source code in help page
+
+15 filtered
+
+issue 2023 proposal 
+
+antismash
+Mobile genetic elements
+Bioinformatics application note
